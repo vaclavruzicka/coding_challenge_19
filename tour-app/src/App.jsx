5c87ba1dd167a940c3b068f7699f791ca991051c
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-04-13 23:41:53 -0400

    Added comments to App.jsx.

--- tour-app/src/App.jsx
+++ tour-app/src/App.jsx
@@ -1,12 +1,15 @@
+//Importing React and useState from react, styles from styles.css, and Gallery component from Gallery.jsx.
 import react, { useState } from 'react'
 import Gallery from './components/Gallery.jsx'
 import './styles/styles.css'
 
+//Creating the App component which is the main component of the application.
 function App() {
 const [tours, setTours] = useState([])
 const removeTour = (id) => {
   setTours((prevTours) => prevTours.filter((tour) => tour.id !== id))
 }
+//Running Gallery component which displays a list of tours from an API.
 return (
   <main>
     <h1> Our Tours</h1>
@@ -15,4 +18,5 @@ return (
 )
 }
 
+//Exporting the App component as the default export of the module.
 export default App
