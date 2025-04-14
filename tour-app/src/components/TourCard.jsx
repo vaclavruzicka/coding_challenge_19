d12738a26ff49dfc5be5201c72193a2fef04dd76
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-04-13 22:07:08 -0400

    Build TourCard with conditional rendering and interaction.

---
+++ tour-app/src/components/TourCard.jsx
@@ -0,0 +1,33 @@
+//TourCard.jsx (Single Tour Component)
+
+//Imports React and useState.
+import React, { useState } from 'react';
+
+//Creating the props for the TourCard component.
+const TourCard = ({id, name, info, price, image, onRemove}) => {
+    const [readMore, setReadMore] = useState(false) //State to manage the read more functionality.
+
+    return (
+        <article className='tour-card'>
+            {/* Creating the tour header with name and price */}
+            <div className='tour-header'>
+                <h3>{name}</h3>
+                <h4>{price}</h4>
+            </div>
+            {/* Adding the image and info section */}
+            <img src={image} alt={name}></img>
+            <p>{readMore ? info : `${info.slice(0,70)}...`}</p>
+            {/* Adding the read more and remove button */}
+            <div className='button-container'>
+                <button className='read-more' onClick={() => setReadMore(!readMore)}>
+                    {readMore ? 'Show Less' : 'Read More'}
+                </button>
+                <button className='remove' onClick={() => onRemove(id)}>
+                    Not Intrested</button>
+            </div>
+        </article>
+    )
+}
+
+//Exporting the TourCard component to be used in other files.
+export default TourCard
